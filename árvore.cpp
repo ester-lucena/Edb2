240d292e2a877629f9f432a06e31dcd07e06641d
Ester Lucena <esterdelucenapaiva@gmail.com>
2025-10-14 14:01:36 -0300

    adicionei um if(exceção) e cout

--- árvore.cpp
+++ árvore.cpp
@@ -22,15 +22,14 @@ struct No{
 //comparador para ordernar do menor para o maior na fila
 struct Comparador {
     bool operator()(No* a, No* b) const {
-        return a->freq > b->freq;
+        return a -> freq > b -> freq;
     }
 };
-
 
 int main(){
     //contador de frequência
     ifstream arquivo("frequencia.txt");
-    if(!arquivo){
+    if(!arquivo) {
         cout << "Erro ao tentar ler o arquivo!" << endl;
         return 1;
     }
@@ -41,28 +40,39 @@ int main(){
     //coloca as informações na fila
     string c;
     int freq;
-    while (arquivo >> c >> freq){
+    while (arquivo >> c >> freq) {
         No* novo = new No(c, freq);
         fila.push(novo);
+    }
+    
+    if (fila.empty()) {
+        cout << "Erro: a fila está vazia. O arquivo pode estar vazio ou mal formatado." << endl;
+        return 1;
     }
 
     //árvore
     while (fila.size() > 1) {
-        //retira os dois menores nós
-        No* esquerda = fila.top(); fila.pop();
-        No* direita = fila.top(); fila.pop();
+        //pega e retira os dois menores nós
+        No* esquerda = fila.top(); 
+        fila.pop();
+        No* direita = fila.top(); 
+        fila.pop();
+        
         //cria o novo nó
-        int soma = esquerda->freq + direita->freq;
+        int soma = esquerda -> freq + direita -> freq;
         No* novo = new No(" ", soma);
+        
         // liga os filhos à esquerda e à direita
-        novo->esquerda = esquerda;
-        novo->direita = direita;
+        novo -> esquerda = esquerda;
+        novo -> direita = direita; 
+        
         //coloca na fila
         fila.push(novo);
     }
 
     //o último nó é a raíz
     No* raiz = fila.top();
+    cout << "Árvore criada com sucesso! Frequência total: " << raiz -> freq << endl;
     
     return 0;
 }
